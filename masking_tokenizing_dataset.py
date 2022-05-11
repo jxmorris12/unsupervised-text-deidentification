update adv masking to pop if answer is incorrect

masking_tokenizing_dataset.py
@@ -74,12 +74,18 @@ class MaskingTokenizingDataset(Dataset):
         else:
             self.masking_span_sampler = None
 
-    def process_grad(self, input_ids: torch.Tensor, emb_grad: torch.Tensor, text_key_id: torch.Tensor) -> None:
+    def process_grad(self,
+            input_ids: torch.Tensor,
+            emb_grad: torch.Tensor,
+            is_correct: torch.Tensor,
+            text_key_id: torch.Tensor
+        ) -> None:
         """Called from model on a training step.
         
         Args:
             input_ids: int torch.Tensor of shape (batch_size, max_seq_length)
             emb_grad: float torch.Tensor of shape (vocab_size,)
+            is_correct: bool torch.Tensor of shape (batch_size,)
             text_key_id: int torch.Tensor of shape (batch_size,)
         """
         if not self.adversarial_masking: 
@@ -92,6 +98,7 @@ class MaskingTokenizingDataset(Dataset):
         batch_size = input_ids.shape[0]
         assert input_ids.shape == (batch_size, self.max_seq_length)
         assert emb_grad.shape == (self.document_tokenizer.vocab_size,)
+        assert is_correct.shape == (batch_size,)
         assert text_key_id.shape == (batch_size,)
 
         # Get word with maximum gradient from each training example.
@@ -115,7 +122,7 @@ class MaskingTokenizingDataset(Dataset):
         token_to_word_map = (
             (~self.subword_map[input_ids]).cumsum(dim=1) - 1
         ).to(emb_grad.device)
-        # example:
+        # example result:
         #        tensor(
         #           [[ 0,  1,  2,  3,  3,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14, 15,
         #            16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
@@ -135,12 +142,17 @@ class MaskingTokenizingDataset(Dataset):
 
         # Store each word so it'll be masked next time.
         for i in range(batch_size):
-            all_word_ids = max_grad_word_ids.cpu()[i]
             ex_index = text_key_id[i].item()
-            subword_ids = all_word_ids[all_word_ids > 0].tolist()
-            assert len(subword_ids) > 0
-            full_word = self.document_tokenizer.decode(subword_ids)
-            self.adv_word_mask_map[ex_index].append(full_word)
+            if is_correct[i].item():
+                # If we got it right, make it harder.
+                all_word_ids = max_grad_word_ids.cpu()[i]
+                subword_ids = all_word_ids[all_word_ids > 0].tolist()
+                assert len(subword_ids) > 0
+                full_word = self.document_tokenizer.decode(subword_ids)
+                self.adv_word_mask_map[ex_index].append(full_word)
+            else:
+                # If we got it wrong, make it easier.
+                self.adv_word_mask_map[ex_index] = self.adv_word_mask_map[ex_index][:-1]
     
     def __len__(self) -> int:
         return len(self.dataset)

model/contrastive.py
@@ -42,6 +42,10 @@ class ContrastiveModel(Model):
             document_to_profile_sim, diagonal_idxs
         )
         self.log(f"{metrics_key}/loss", loss)
+
+        # Also track a boolean mask for which things were correct.
+        is_correct = (document_to_profile_sim.argmax(dim=1) == diagonal_idxs)
+
         # Log top-k accuracies.
         for k in [1, 5, 10, 50, 100, 500, 1000]:
             if k >= batch_size: # can't compute top-k accuracy here.
@@ -55,7 +59,7 @@ class ContrastiveModel(Model):
                     .mean()
             )
             self.log(f"{metrics_key}/acc_top_k/{k}", top_k_acc)
-        return document_to_profile_sim, loss
+        return document_to_profile_sim, is_correct, loss
     
     def compute_loss(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> Dict[str, torch.Tensor]:
         document_embeddings = self.forward_document(
@@ -75,7 +79,7 @@ class ContrastiveModel(Model):
             profile_embeddings = torch.cat(
                 (profile_embeddings, extra_profile_embeddings), dim=0
             )
-            document_to_profile_sim, loss = self._compute_loss_infonce(
+            document_to_profile_sim, is_correct, loss = self._compute_loss_infonce(
                 document_embeddings=document_embeddings,
                 profile_embeddings=profile_embeddings,
                 metrics_key='train'
@@ -85,7 +89,7 @@ class ContrastiveModel(Model):
             # TODO:: somehow update nearest-neighbors here to take top-K of document2profilesim along axis 1.
             # self.trainer.datamodule.compute_new_nearest_neighbors(document_to_profile_sim, idxs)
         else:
-            _, loss = self._compute_loss_infonce(
+            _, is_correct, loss = self._compute_loss_infonce(
                 document_embeddings=document_embeddings,
                 profile_embeddings=profile_embeddings,
                 metrics_key='train'
@@ -94,6 +98,7 @@ class ContrastiveModel(Model):
             "loss": loss,
             "document_embeddings": document_embeddings.detach().cpu(),
             "profile_embeddings": profile_embeddings.detach().cpu(),
+            "is_correct": is_correct.cpu(),
             "text_key_id": batch['text_key_id'].cpu()
         }
 

model/model.py
@@ -146,6 +146,10 @@ class Model(LightningModule, abc.ABC):
             document_to_profile_sim, document_idxs
         )
         self.log(f"{metrics_key}/loss", loss)
+
+        # Also track a boolean mask for which things were correct.
+        is_correct = (document_to_profile_sim.argmax(dim=1) == document_idxs)
+
         # Log top-k accuracies.
         for k in [1, 5, 10, 50, 100, 500, 1000]:
             if k >= batch_size: # can't compute top-k accuracy here.
@@ -159,7 +163,7 @@ class Model(LightningModule, abc.ABC):
                     .mean()
             )
             self.log(f"{metrics_key}/acc_top_k/{k}", top_k_acc)
-        return loss
+        return is_correct, loss
     
     def forward_document_inputs(self, inputs: Dict[str, torch.Tensor]) -> torch.Tensor:
         inputs = {k: v.to(self.document_model_device) for k,v in inputs.items()}
@@ -221,13 +225,17 @@ class Model(LightningModule, abc.ABC):
         torch.nn.utils.clip_grad_norm_(self.parameters(), self.grad_norm_clip)
         optimizer.step()
 
-        # We call process_grad() on the train dataset because the
-        # train dataset may use this gradient to perform masking.
-        self.trainer.train_dataloader.loaders.dataset.process_grad(
-          input_ids=batch['document__input_ids'],
-          emb_grad=self.document_model.embeddings.word_embeddings.weight.grad.norm(p=2, dim=1),
-          text_key_id=batch['text_key_id']
-        )
+        emb_grad = self.document_model.embeddings.word_embeddings.weight.grad
+
+        if not (emb_grad is None):
+            # We call process_grad() on the train dataset because the
+            # train dataset may use this gradient to perform masking.
+            self.trainer.train_dataloader.loaders.dataset.process_grad(
+                input_ids=batch['document__input_ids'],
+                emb_grad=emb_grad.norm(p=2, dim=1),
+                is_correct=results["is_correct"],
+                text_key_id=batch['text_key_id']
+            )
         return results
     
     def _process_validation_batch(self, batch: Dict[str, Any], batch_idx: int) -> Dict[str, torch.Tensor]:
@@ -315,15 +323,15 @@ class Model(LightningModule, abc.ABC):
         scheduler = self.get_scheduler()
 
         # Compute losses on regular documents.
-        doc_loss = self._compute_loss_exact(
+        _, doc_loss = self._compute_loss_exact(
             document_embeddings.cuda(), profile_embeddings.cuda(), text_key_id.cuda(),
             metrics_key='val/document'
         )
-        doc_redact_ner_loss = self._compute_loss_exact(
+        _, doc_redact_ner_loss = self._compute_loss_exact(
             document_redact_ner_embeddings.cuda(), profile_embeddings.cuda(), text_key_id.cuda(),
             metrics_key='val/document_redact_ner'
         )
-        doc_redact_lexical_loss = self._compute_loss_exact(
+        _, doc_redact_lexical_loss = self._compute_loss_exact(
             document_redact_lexical_embeddings.cuda(), profile_embeddings.cuda(), text_key_id.cuda(),
             metrics_key='val/document_redact_lexical'
         )