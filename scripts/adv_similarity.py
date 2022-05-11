3d5bdb660fafccbaa43ed4588f8b3b6809f3bbef
Jack Morris <jxmorris12@gmail.com>
2022-05-11 12:58:48 -0400

    dont dropout stopwords

--- scripts/adv_similarity.py
+++ scripts/adv_similarity.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 
 import os
 import pickle
@@ -21,6 +21,9 @@ def get_words_from_doc(s: List[str]) -> List[str]:
     words = s.split()
     return [w for w in words if not w in eng_stopwords]
 
+def combine_dicts(a: Dict[str, int], b: Dict[str, int]) -> Dict[str, int]:
+    all_keys = a.keys() | b.keys()
+    return {k: a.get(k, 0) + b.get(k, 0) for k in all_keys}
 
 def get_top_matches_adv_data_bm25():
     split = 'val[:20%]'
@@ -34,30 +37,54 @@ def get_top_matches_adv_data_bm25():
         return ex
 
     train_data = train_data.map(make_table_str)
-    corpus = train_data['table_str']
+    profile_corpus = train_data['table_str']
+    document_corpus = train_data['target_text']
 
-    print("tokenizing corpus")
-    tokenized_corpus = [
-        get_words_from_doc(doc) for doc in corpus
+    print("tokenizing corpii")
+    tokenized_document_corpus = [
+        get_words_from_doc(doc) for doc in document_corpus
+    ]
+    tokenized_profile_corpus = [
+        get_words_from_doc(prof) for prof in profile_corpus
     ]
 
     print("creating search index")
-    bm25 = BM25Okapi(tokenized_corpus)
+
+    bm25 = BM25Okapi(tokenized_profile_corpus)
+
+    ######################################################################
+    # combine corpuses for idf calculation
+    # doc_bm25 = BM25Okapi(tokenized_document_corpus)
+    # ndd = doc_bm25._initialize(tokenized_document_corpus)
     
-    adv_csv_filename = 'adv_csvs/model_2/results_1_1000.csv'
+    # prof_bm25 = BM25Okapi(tokenized_profile_corpus)
+    # ndp = prof_bm25._initialize(tokenized_profile_corpus)
+
+    # bm25 = BM25Okapi(tokenized_profile_corpus)
+    # bm25._calc_idf(combine_dicts(ndd, ndp))
+    # bm25.avgdl = (bm25.avgdl + doc_bm25.avgdl) / 2.0
+    # bm25.doc_freqs = combine_dicts(bm25.doc_freqs, doc_bm25.doc_freqs)
+    ######################################################################
+
+    # adv_csv_filename = 'adv_csvs/model_1/results_1_1000.csv' # 0.670 (prof corpus, removed stopwords)
+    # adv_csv_filename = 'adv_csvs/model_1/results_10_1000.csv' # 0.397 (prof corpus, removed stopwords)
+    adv_csv_filename = 'adv_csvs/model_1/results_100_1000.csv' # 0.397 (prof corpus, removed stopwords)
+    # adv_csv_filename = 'adv_csvs/model_2/results_1_1000.csv' # 0.469 (prof corpus, removed stopwords)
+    # adv_csv_filename = 'adv_csvs/model_2/results_10_1000.csv' # 0.284 (prof corpus, removed stopwords)
+
     print(f"loading adversarial data from {adv_csv_filename}")
     
     adv_df = pd.read_csv(adv_csv_filename)
     adv_df["adv_idx"] = pickle.load(open('nearest_neighbors/nn__idxs.p', 'rb'))
     adv_df = adv_df[adv_df["result_type"] == "Successful"]
 
-    ######################################################################
+    # ######################################################################
     # idx = 14
     # ex = adv_df.iloc[idx]
     # query = ex["perturbed_text"].split()
     # top_k = bm25.get_scores(query).argsort()[::-1][:1+k]
     # breakpoint()
-    ######################################################################
+    ########################################################################
 
     print("getting top-k matches")
     top_matches = []
