fix adv-masking using dataloader call to word_ids()

main.py
@@ -20,7 +20,7 @@ USE_WANDB = True
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
-num_cpus = len(os.sched_getaffinity(0))
+num_cpus = 0#len(os.sched_getaffinity(0))
 
 
 def get_args() -> argparse.Namespace:
@@ -29,11 +29,16 @@ def get_args() -> argparse.Namespace:
         formatter_class=argparse.ArgumentDefaultsHelpFormatter
     )
 
+    parser.add_argument('--checkpoint_path', type=str, default='')
+
     parser.add_argument('--loss_function', '--loss_fn', '--loss', type=str,
         choices=['coordinate_ascent', 'contrastive'],
         default='coordinate_ascent',
         help='loss function to use for training'
     )
+
+    parser.add_argument('--limit_val_batches', type=float, default=1.0,
+        help='\% of validation to use. ONLY reduce this for debugging.')
 
     parser.add_argument('--num_validations_per_epoch', type=int, default=1,
         help='number of times to validate per epoch')
@@ -129,9 +134,8 @@ def main(args: argparse.Namespace):
     model_cls = model_cls_dict[args.loss_function]
 
     # roberta-tapas trained on 0.5/0.5/0.5 dropout for 110 epochs /22 hours:
-    checkpoint_path = "/home/jxm3/research/deidentification/unsupervised-deidentification/saves/ca__roberta__tapas__dropout_0.5_0.5_0.5/deid-wikibio-2_default/2ai8js2r_328/checkpoints/epoch=110-step=50615.ckpt"
-
-    # checkpoint_path = None
+    # checkpoint_path = "/home/jxm3/research/deidentification/unsupervised-deidentification/saves/ca__roberta__tapas__dropout_0.5_0.5_0.5/deid-wikibio-2_default/2ai8js2r_328/checkpoints/epoch=110-step=50615.ckpt"
+    checkpoint_path = args.checkpoint_path
 
     if checkpoint_path:
         model = model_cls.load_from_checkpoint(checkpoint_path)
@@ -160,7 +164,7 @@ def main(args: argparse.Namespace):
     if args.sample_spans:
         exp_name += f'__sample_spans'
     if args.adversarial_masking:
-        exp_name += f'__adv_{args.adversarial_masking}'
+        exp_name += f'__adv'
     if args.num_nearest_neighbors:
         exp_name += f'__n_{args.num_nearest_neighbors}'
     if args.word_dropout_ratio or args.profile_row_dropout_perc:
@@ -184,7 +188,7 @@ def main(args: argparse.Namespace):
             job_type='train',
             entity='jack-morris',
         )
-        wandb_logger.watch(model)
+        wandb_logger.watch(model, log_graph=True)
         loggers.append(
             wandb_logger
         )
@@ -214,8 +218,8 @@ def main(args: argparse.Namespace):
         callbacks=callbacks,
         max_epochs=args.epochs,
         log_every_n_steps=min(len(dm.train_dataloader()), 50),
-        limit_train_batches=1.0, # change this to make training faster (1.0 = full train set)
-        limit_val_batches=2,
+        # limit_train_batches=1.0, # change this to make training faster (1.0 = full train set)
+        limit_val_batches=args.limit_val_batches,
         gpus=torch.cuda.device_count(),
         logger=loggers,
     )

model/coordinate_ascent.py
@@ -18,7 +18,8 @@ class CoordinateAscentModel(Model):
     
     def _precompute_profile_embeddings(self):
         self.profile_model.cuda()
-        self.profile_model.eval()
+        # self.profile_model.eval()
+        self.profile_model.train()
         print(f'Precomputing profile embeddings at epoch {self.current_epoch}...')
         self.train_profile_embeddings = np.zeros((len(self.trainer.datamodule.train_dataset), self.profile_embedding_dim))
         for train_batch in tqdm.tqdm(self.trainer.datamodule.train_dataloader(), desc="Precomputing train embeddings", colour="magenta", leave=False):
@@ -30,7 +31,8 @@ class CoordinateAscentModel(Model):
     
     def _precompute_document_embeddings(self):
         self.document_model.cuda()
-        self.document_model.eval()
+        # self.document_model.eval()
+        self.document_model.train()
         print(f'Precomputing document embeddings at epoch {self.current_epoch}...')
         self.train_document_embeddings = np.zeros((len(self.trainer.datamodule.train_dataset), self.profile_embedding_dim))
         for train_batch in tqdm.tqdm(self.trainer.datamodule.train_dataloader(), desc="Precomputing train embeddings", colour="magenta", leave=False):

model/model.py
@@ -93,17 +93,19 @@ class Model(LightningModule, abc.ABC):
             return
 
         train_dataset = self.trainer.train_dataloader.loaders.dataset
+
         if train_dataset.adversarial_masking:
             rows = []
-            for idx in range(20):
-                doc = train_dataset.dataset[idx]
+            for idx in range(8):
+                doc_name = train_dataset.dataset[idx]["name"]
+                doc_text = train_dataset.dataset[idx]["document"]
                 masked_words = train_dataset.adv_word_mask_map[idx]
-                rows.append((doc, masked_words))
-                my_table = wandb.Table(
-                    columns=["document", "masked words"],
-                    data=rows
-                )
-                wandb.run.log({"adversarial_mask_table": my_table})
+                rows.append((doc_name, doc_text, masked_words))
+            my_table = wandb.Table(
+                columns=["name", "document", "masked words"],
+                data=rows
+            )
+            wandb.run.log({"adversarial_mask_table": my_table})
 
     def on_train_epoch_end(self):
         self._log_adv_masking_table()
@@ -166,6 +168,11 @@ class Model(LightningModule, abc.ABC):
         return is_correct, loss
     
     def forward_document_inputs(self, inputs: Dict[str, torch.Tensor]) -> torch.Tensor:
+        # We track the word IDs for documents in case we need them to do
+        # subword pooling or anything like that. But the document doesn't take
+        # word_ids as input to the forward pass.
+        if "word_ids" in inputs: del inputs["word_ids"]
+
         inputs = {k: v.to(self.document_model_device) for k,v in inputs.items()}
         document_outputs = self.document_model(**inputs)                        # (batch,  sequence_length) -> (batch, sequence_length, document_emb_dim)
         document_embeddings = document_outputs['last_hidden_state'][:, 0, :]    # (batch, document_emb_dim)
@@ -208,30 +215,28 @@ class Model(LightningModule, abc.ABC):
         raise NotImplementedError()
 
     def training_step(self, batch: Dict[str, torch.Tensor], batch_idx: int) -> torch.Tensor:
-        """
-        dict_keys(['document', 'profile', 'document_redact_lexical', 'document_redact_ner', 'text_key_id'])
-        """
         # Alternate between training phases per epoch.
         assert self.document_model.training
         assert self.document_embed.training
         assert self.profile_model.training
 
         optimizer = self.get_optimizer()
+        optimizer.zero_grad()
         results = self.compute_loss(batch=batch, batch_idx=batch_idx)
         self.log("temperature", self.temperature.exp())
 
-        optimizer.zero_grad()
         self.manual_backward(results["loss"])
         torch.nn.utils.clip_grad_norm_(self.parameters(), self.grad_norm_clip)
         optimizer.step()
 
         emb_grad = self.document_model.embeddings.word_embeddings.weight.grad
 
-        if not (emb_grad is None):
+        if not (emb_grad is None) and (emb_grad.sum() > 0):
             # We call process_grad() on the train dataset because the
             # train dataset may use this gradient to perform masking.
             self.trainer.train_dataloader.loaders.dataset.process_grad(
                 input_ids=batch['document__input_ids'],
+                word_ids=batch['document__word_ids'],
                 emb_grad=emb_grad.norm(p=2, dim=1),
                 is_correct=results["is_correct"],
                 text_key_id=batch['text_key_id']