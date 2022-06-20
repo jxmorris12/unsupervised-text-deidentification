diagnose masking bug

model/model.py
@@ -182,18 +182,18 @@ class Model(LightningModule, abc.ABC):
         is_correct = (document_to_profile_sim.argmax(dim=1) == document_idxs)
 
         # Log top-k accuracies.
-        for k in [1, 5, 10, 50, 100, 500, 1000]:
-            if k >= batch_size: # can't compute top-k accuracy here.
-                continue
-            top_k_acc = (
-                document_to_profile_sim.topk(k=k, dim=1)
-                    .indices
-                    .eq(document_idxs[:, None])
-                    .any(dim=1)
-                    .float()
-                    .mean()
-            )
-            self.log(f"{metrics_key}/acc_top_k/{k}", top_k_acc)
+        # for k in [1, 5, 10, 50, 100, 500, 1000]:
+        #     if k >= batch_size: # can't compute top-k accuracy here.
+        #         continue
+        #     top_k_acc = (
+        #         document_to_profile_sim.topk(k=k, dim=1)
+        #             .indices
+        #             .eq(document_idxs[:, None])
+        #             .any(dim=1)
+        #             .float()
+        #             .mean()
+        #     )
+        #     self.log(f"{metrics_key}/acc_top_k/{k}", top_k_acc)
         return is_correct, loss
     
     def forward_document_inputs(self, inputs: Dict[str, torch.Tensor]) -> torch.Tensor:
@@ -437,6 +437,8 @@ class Model(LightningModule, abc.ABC):
             )
 
         # Compute losses on regular + redacted documents.
+        # TODO - make work on multi-gpu like this guy did: https://github.com/Lightning-AI/lightning/issues/4175
+        #   (Can test with 4 GPUs on 1% train/val data.)
         document_embeddings = torch.cat(
             [o['document_embeddings'] for o in val_outputs], axis=0)
         _, doc_loss = self._compute_loss_exact(

scripts/birthday_probing.py
@@ -22,6 +22,7 @@ from tqdm import tqdm
 from dataloader import WikipediaDataModule
 from model import CoordinateAscentModel
 from model_cfg import model_paths_dict
+from utils import get_profile_embeddings_by_model_key
 
 num_cpus = len(os.sched_getaffinity(0))
 USE_WANDB = False
@@ -52,7 +53,7 @@ def get_args() -> argparse.Namespace:
     
     parser.add_argument('--model_key', type=str, default='model_8_ls0.1', help='path to model key (see model_cfg.py)')
     parser.add_argument('--dataset_train_split', type=str, default='train[:10%]')
-    parser.add_argument('--num_validations_per_epoch', type=int, default=4)
+    parser.add_argument('--num_validations_per_epoch', type=int, default=1)
 
     parser.add_argument('--epochs', type=int, default=100)
     parser.add_argument('--batch_size', type=int, default=2048)
@@ -65,11 +66,13 @@ def get_args() -> argparse.Namespace:
 
 class BirthdayDataModule(LightningDataModule):
     train_dataset: List[Tuple[int, int, int]]
+    test_dataset: List[Tuple[int, int, int]]
     val_dataset: List[Tuple[int, int, int]]
     batch_size: int
     def __init__(self, dm: WikipediaDataModule, batch_size: int = 128):
         super().__init__()
         self.train_dataset = process_dataset(dm.train_dataset)
+        self.test_dataset = process_dataset(dm.test_dataset)
         self.val_dataset = process_dataset(dm.val_dataset)
         self.batch_size = batch_size
         self.num_workers = min(4, num_cpus)
@@ -82,6 +85,14 @@ class BirthdayDataModule(LightningDataModule):
             self.train_dataset,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
+            shuffle=True
+        )
+
+    def test_dataloader(self) -> DataLoader:
+        return DataLoader(
+            self.test_dataset,
+            batch_size=self.batch_size,
+            num_workers=self.num_workers,
             shuffle=False # Only shuffle for train
         )
 
@@ -90,27 +101,35 @@ class BirthdayDataModule(LightningDataModule):
             self.val_dataset,
             batch_size=self.batch_size,
             num_workers=self.num_workers,
-            shuffle=False
+            shuffle=False # Only shuffle for train
         )
 
 
 class BirthdayModel(LightningModule):
     """Probes the PROFILE for birthday info."""
-    profile_embeddings: torch.Tensor
+    train_profile_embeddings: torch.Tensor
+    test_profile_embeddings: torch.Tensor
+    val_profile_embeddings: torch.Tensor
     classifier: torch.nn.Module
     learning_rate: float
     
-    def __init__(self, model: CoordinateAscentModel, learning_rate: float):
+    def __init__(self, profile_embeddings: Dict[str, torch.Tensor], learning_rate: float):
         super().__init__()
-        # We can pre-calculate these embeddings bc
-        self.train_profile_embeddings = torch.tensor(model.train_profile_embeddings.cpu())
-        self.val_profile_embeddings = torch.tensor(model.val_profile_embeddings.cpu())
+
+        self.train_profile_embeddings = profile_embeddings['train']
+        self.val_profile_embeddings = profile_embeddings['val']
+        self.test_profile_embeddings = profile_embeddings['test']
+
+        embedding_dim = self.train_profile_embeddings.shape[1]
+
         self.month_classifier = torch.nn.Sequential(
-            torch.nn.Linear(model.shared_embedding_dim, 64),
+            torch.nn.Linear(embedding_dim, 64),
+            torch.nn.ReLU(),
             torch.nn.Linear(64, 12),
         )
         self.day_classifier = torch.nn.Sequential(
-            torch.nn.Linear(model.shared_embedding_dim, 64),
+            torch.nn.Linear(embedding_dim, 64),
+            torch.nn.ReLU(),
             torch.nn.Linear(64, 31),
         )
         self.learning_rate = learning_rate
@@ -170,8 +189,8 @@ class BirthdayModel(LightningModule):
         self.log('val_acc_day', self.val_accuracy(day_logits, days))
         
         if batch_idx == 0:
-            print('val_acc_month', self.val_accuracy(month_logits, months))
-            print('val_acc_day', self.val_accuracy(day_logits, days))
+            self._print(f'Epoch {self.current_epoch}  val_acc_month = {self.val_accuracy(month_logits, months)}')
+            self._print(f'Epoch {self.current_epoch}  val_acc_day = {self.val_accuracy(day_logits, days)}')
 
         return (month_loss + day_loss)
 
@@ -181,28 +200,6 @@ class BirthdayModel(LightningModule):
             list(self.parameters()), lr=self.learning_rate
         )
         return optimizer
-            
-
-def precompute_profile_embeddings(model: CoordinateAscentModel, dm: WikipediaDataModule):
-    model.profile_model.cuda()
-    model.profile_model.eval()
-    model.profile_embed.cuda()
-    model.profile_embed.eval()
-
-
-    model.train_profile_embeddings = np.zeros((len(dm.train_dataset), model.shared_embedding_dim))
-    for train_batch in tqdm(dm.train_dataloader(), desc="Precomputing train embeddings", colour="#008080", leave=False):
-        with torch.no_grad():
-            profile_embeddings = model.forward_profile(batch=train_batch)
-        model.train_profile_embeddings[train_batch["text_key_id"]] = profile_embeddings.cpu()
-    model.train_profile_embeddings = torch.tensor(model.train_profile_embeddings, dtype=torch.float32)
-    
-    model.val_profile_embeddings = np.zeros((len(dm.val_dataset), model.shared_embedding_dim))
-    for val_batch in tqdm(dm.val_dataloader()[0], desc="Precomputing val embeddings", colour="#c09591", leave=False):
-        with torch.no_grad():
-            profile_embeddings = model.forward_profile(batch=val_batch)
-        model.val_profile_embeddings[val_batch["text_key_id"]] = profile_embeddings.cpu()
-    model.val_profile_embeddings = torch.tensor(model.val_profile_embeddings, dtype=torch.float32)
 
 
 def main(args: argparse.Namespace):
@@ -213,26 +210,29 @@ def main(args: argparse.Namespace):
     checkpoint_path = model_paths_dict[args.model_key]
     print(checkpoint_path)
 
-    model = CoordinateAscentModel.load_from_checkpoint(
-        checkpoint_path
-    )
+    model = CoordinateAscentModel.load_from_checkpoint(checkpoint_path)
 
     dm = WikipediaDataModule(
-        document_model_name_or_path=model.document_model_name_or_path,
-        profile_model_name_or_path=model.profile_model_name_or_path,
+        document_model_name_or_path='roberta-base', # these don't matter for probing (we just use raw profiles/documents)
+        profile_model_name_or_path='google/tapas-base',# these don't matter for probing (we just use raw profiles/documents)
         max_seq_length=128,
         dataset_name='wiki_bio',
         dataset_train_split=args.dataset_train_split,
         dataset_val_split=args.dataset_val_split,
         dataset_version='1.2.0',
         num_workers=num_cpus,
-        train_batch_size=64,
-        eval_batch_size=64
     )
     dm.setup("fit")
     
-    precompute_profile_embeddings(model=model, dm=dm)
-    birthday_model = BirthdayModel(model, args.learning_rate)
+    
+    profile_embeddings = get_profile_embeddings_by_model_key(model_key=args.model_key)
+
+    print("concatenating train, val, and test profile embeddings")
+
+    birthday_model = BirthdayModel(
+        profile_embeddings=profile_embeddings,
+        learning_rate=args.learning_rate
+    )
     
     birthday_dm = BirthdayDataModule(dm)
     birthday_dm.batch_size = args.batch_size

scripts/generate_masked_samples.py
@@ -35,8 +35,10 @@ num_cpus = len(os.sched_getaffinity(0))
 
 class ChangeClassificationToBelowTopKClasses(textattack.goal_functions.ClassificationGoalFunction):
     k: int
-    def __init__(self, *args, k: int = 1, **kwargs):
+    normalize_scores: bool
+    def __init__(self, *args, k: int = 1, normalize_scores: bool, **kwargs):
         self.k = k
+        self.normalize_scores = normalize_scores
         super().__init__(*args, **kwargs)
 
     def _is_goal_complete(self, model_output, _):
@@ -44,8 +46,14 @@ class ChangeClassificationToBelowTopKClasses(textattack.goal_functions.Classific
         num_better_classes = (model_output > original_class_score).sum()
         return num_better_classes >= self.k
 
-    def _get_score(self, model_output, _):
-        return 1 - model_output[self.ground_truth_output]
+    def _get_score(self, model_logits, _):
+        if self.normalize_scores:
+            model_probs = torch.nn.functional.softmax(
+                model_logits, dim=-1
+            )
+            return 1 - model_probs[self.ground_truth_output]
+        else:
+            return -1 * model_logits[self.ground_truth_output]
     
     
     """have to reimplement the following method to change the precision on the sum-to-one condition."""
@@ -84,7 +92,7 @@ class ChangeClassificationToBelowTopKClasses(textattack.goal_functions.Classific
             raise ValueError(
                 f"Model return score of shape {scores.shape} for {len(inputs)} inputs."
             )
-        elif not ((scores.sum(dim=1) - 1).abs() < 1e-4).all():
+        elif self.normalize_scores and (not ((scores.sum(dim=1) - 1).abs() < 1e-4).all()):
             # Values in each row should sum up to 1. The model should return a
             # set of numbers corresponding to probabilities, which should add
             # up to 1. Since they are `torch.float` values, allow a small
@@ -133,10 +141,12 @@ class WikiDataset(textattack.datasets.Dataset):
     dataset: List[Dict[str, str]]
     label_names: List[str]
     
-    def __init__(self, dm: WikipediaDataModule):
+    def __init__(self, dm: WikipediaDataModule, max_samples: int = 1000):
         self.shuffled = True
         # filter out super long examples
-        self.dataset = [ex for ex in dm.test_dataset]
+        self.dataset = [
+            dm.test_dataset[i] for i in range(max_samples)
+        ]
         self.label_names = np.array(list(dm.test_dataset['name']) + list(dm.val_dataset['name']) + list(dm.train_dataset['name']))
     
     def __len__(self) -> int:
@@ -186,11 +196,9 @@ class MyModelWrapper(textattack.models.wrappers.ModelWrapper):
         with torch.no_grad():
             document_embeddings = self.model.forward_document(batch=batch, document_type='document')
             document_to_profile_logits = document_embeddings @ (self.profile_embeddings.T)
-            document_to_profile_probs = torch.nn.functional.softmax(
-                document_to_profile_logits, dim=-1
-            )
-        assert document_to_profile_probs.shape == (len(text_input_list), len(self.profile_embeddings))
-        return document_to_profile_probs
+        breakpoint()
+        assert document_to_profile_logits.shape == (len(text_input_list), len(self.profile_embeddings))
+        return document_to_profile_logits
 
 
 def get_profile_embeddings(model_key: str):
@@ -205,7 +213,7 @@ def get_profile_embeddings(model_key: str):
     return all_profile_embeddings
 
 
-def main(k: int, n: int, beam_width: int, model_key: str):
+def main(k: int, n: int, num_examples_offset: int, beam_width: int, model_key: str):
     checkpoint_path = model_paths_dict[model_key]
     assert isinstance(checkpoint_path, str), f"invalid checkpoint_path {checkpoint_path} for {model_key}"
     print(f"running attack on {model_key} loaded from {checkpoint_path}")
@@ -250,11 +258,15 @@ def main(k: int, n: int, beam_width: int, model_key: str):
     search_method = textattack.search_methods.BeamSearch(beam_width=beam_width)
 
     print(f'***Attacking with k={k} n={n}***')
-    goal_function = ChangeClassificationToBelowTopKClasses(model_wrapper, k=k)
+    goal_function = ChangeClassificationToBelowTopKClasses(model_wrapper, k=k, normalize_scores=False)
     attack = Attack(
         goal_function, constraints, transformation, search_method
     )
-    attack_args = AttackArgs(num_examples=n, disable_stdout=False)
+    attack_args = AttackArgs(
+        num_examples_offset=num_examples_offset,
+        num_examples=n,
+        disable_stdout=False
+    )
     attacker = Attacker(attack, dataset, attack_args)
 
     results_iterable = attacker.attack_dataset()
@@ -264,9 +276,11 @@ def main(k: int, n: int, beam_width: int, model_key: str):
     for result in results_iterable:
         logger.log_attack_result(result)
 
-    folder_path = os.path.join('adv_csvs_full', model_key)
-    os.makedirs(folder_path, exist_ok=True)
-    logger.df.to_csv(os.path.join(folder_path, f'results__b_{beam_width}__k_{k}__n_{n}.csv'))
+    # folder_path = os.path.join('adv_csvs_full', model_key)
+    # os.makedirs(folder_path, exist_ok=True)
+    # out_csv_path = os.path.join(folder_path, f'results__b_{beam_width}__k_{k}__n_{n}.csv')
+    # logger.df.to_csv(out_csv_path)
+    # print('wrote csv to', out_csv_path)
     
 
 def get_args() -> argparse.Namespace:
@@ -280,10 +294,13 @@ def get_args() -> argparse.Namespace:
     parser.add_argument('--n', type=int, default=1000,
         help='number of examples to run on'
     )
-    parser.add_argument('--beam_width', '--b', type=int, default=4,
+    parser.add_argument('--num_examples_offset', type=int, default=0,
+        help='offset for search'
+    )
+    parser.add_argument('--beam_width', '--b', type=int, default=1,
         help='beam width for beam search'
     )
-    parser.add_argument('--model', '--model_key', type=str, default='model_5',
+    parser.add_argument('--model', '--model_key', type=str, default='model_3_1',
         help='model str name (see model_cfg for more info)',
         choices=model_paths_dict.keys()
     )
@@ -294,4 +311,4 @@ def get_args() -> argparse.Namespace:
 
 if __name__ == '__main__':
     args = get_args()
-    main(k=args.k, n=args.n, beam_width=args.beam_width, model_key=args.model)
+    main(k=args.k, n=args.n, num_examples_offset=args.num_examples_offset, beam_width=args.beam_width, model_key=args.model)

utils/embedding.py
@@ -30,21 +30,21 @@ def precompute_profile_embeddings(
     model.profile_embed.eval()
 
     train_profile_embeddings = np.zeros((len(dm.train_dataset), model.shared_embedding_dim))
-    for val_batch in tqdm(dm.train_dataloader(), desc="Precomputing train embeddings", colour="blue", leave=False):
+    for val_batch in tqdm(dm.train_dataloader(), desc="Precomputing train profile embeddings", colour="blue", leave=False):
         with torch.no_grad():
             profile_embeddings = model.forward_profile(batch=val_batch)
         train_profile_embeddings[val_batch["text_key_id"]] = profile_embeddings.cpu()
     train_profile_embeddings = torch.tensor(train_profile_embeddings, dtype=torch.float32)
 
     val_profile_embeddings = np.zeros((len(dm.val_dataset), model.shared_embedding_dim))
-    for val_batch in tqdm(dm.val_dataloader()[0], desc="Precomputing val embeddings", colour="green", leave=False):
+    for val_batch in tqdm(dm.val_dataloader()[0], desc="Precomputing val profile embeddings", colour="green", leave=False):
         with torch.no_grad():
             profile_embeddings = model.forward_profile(batch=val_batch)
         val_profile_embeddings[val_batch["text_key_id"]] = profile_embeddings.cpu()
     val_profile_embeddings = torch.tensor(val_profile_embeddings, dtype=torch.float32)
 
     test_profile_embeddings = np.zeros((len(dm.test_dataset), model.shared_embedding_dim))
-    for test_batch in tqdm(dm.test_dataloader(), desc="Precomputing test embeddings", colour="magenta", leave=False):
+    for test_batch in tqdm(dm.test_dataloader(), desc="Precomputing test profile embeddings", colour="magenta", leave=False):
         with torch.no_grad():
             profile_embeddings = model.forward_profile(batch=test_batch)
         test_profile_embeddings[test_batch["text_key_id"]] = profile_embeddings.cpu()