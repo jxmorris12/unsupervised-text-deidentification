453cfb6145e43b4be1014e322df9fca4e3cb6b58
Jack Morris <jxmorris12@gmail.com>
2022-06-21 23:03:42 -0400

    more CSVs, notebook with pareto plots, generate sequential examples

---
+++ scripts/generate_masked_samples_sequential.py
@@ -0,0 +1,64 @@
+import sys
+sys.path.append('/home/jxm3/research/deidentification/unsupervised-deidentification')
+
+import argparse
+import os
+import re
+
+import pandas as pd
+
+from model_cfg import model_paths_dict
+from generate_masked_samples import WikiDataset, main
+
+
+def get_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description=(
+            'Generate adversarially-masked examples for a model, continued off '
+            'of previous set of adversarial examples.'
+        ),
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument('--k', type=int, default=1,
+        help='top-K classes for adversarial goal function'
+    )
+    parser.add_argument('csv_path', type=str,
+        help='CSV to use as starting point for adversarial redaction generation'
+    )
+    parser.add_argument('--model', '--model_key', type=str, default='model_3_1',
+        help='model str name (see model_cfg for more info)',
+        choices=model_paths_dict.keys()
+    )
+    parser.add_argument('--beam_width', '--b', type=int, default=1,
+        help='beam width for beam search'
+    )
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == '__main__':
+    args = get_args()
+
+    assert args.csv_path.endswith('.csv')
+
+    n = int(re.search(r'.+__n_(\d+).*.csv', args.csv_path).group(1))
+    use_train_profiles = 'with_train' in args.csv_path
+    out_folder_path = os.path.join(
+        args.csv_path.replace('.csv', ''), 'sequential', args.model
+    )
+    os.makedirs(out_folder_path, exist_ok=True)
+
+    dataset = pd.read_csv(args.csv_path)
+
+    main(
+        k=args.k,
+        n=n,
+        num_examples_offset=0,
+        beam_width=args.beam_width,
+        dataset=dataset,
+        model_key=args.model,
+        use_type_swap=False,
+        use_train_profiles=use_train_profiles,
+        out_folder_path=out_folder_path,
+    )
